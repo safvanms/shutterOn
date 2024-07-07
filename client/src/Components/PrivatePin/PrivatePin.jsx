492d1ef6245444af30be38eec374804271942077
Muhammed Safvan <103893917+safvanms@users.noreply.github.com>
2024-07-08 00:26:02 +0530

    Update PrivatePin.jsx

--- client/src/Components/PrivatePin/PrivatePin.jsx
+++ client/src/Components/PrivatePin/PrivatePin.jsx
@@ -47,7 +47,7 @@ const PrivatePin = ({
           {pin.map((otpValue, index) => {
             return (
               <input
-                type="number"
+                type="tel"
                 key={index}
                 value={otpValue}
                 ref={(input) => (inputRef.current[index] = input)}
