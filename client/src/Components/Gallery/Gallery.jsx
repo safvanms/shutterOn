d1f0dde99744b408f80af26224cd19c5d10cf6a0
safvan <m.safvan@linkshadow.com>
2025-06-24 20:13:56 +0530

    UI changes

--- client/src/Components/Gallery/Gallery.jsx
+++ client/src/Components/Gallery/Gallery.jsx
@@ -8,6 +8,9 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import PrivatePin from "../PrivatePin/PrivatePin";
 import Masonry from "react-masonry-css";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Gallery = () => {
   const [galleryImages, setGalleryImages] = useState([]);
@@ -95,30 +98,33 @@ const Gallery = () => {
   };
 
 
-  const handleDownload = async (imageUrl) => {
-    try {
-      const response = await fetch(imageUrl, { mode: "cors" });
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
+ const handleDownload = async (imageUrl) => {
+  try {
+    const response = await fetch(imageUrl, { mode: "cors" });
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
 
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = `photo_${Date.now()}.jpg`;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `photo_${Date.now()}.jpg`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 
-      // Optional: Cleanup
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error("Download failed:", error);
-      alert("Failed to download image. Please try again.");
-    }
-  };
+    window.URL.revokeObjectURL(url);
+
+    toast.success("Image downloaded successfully!");
+  } catch (error) {
+    console.error("Download failed:", error);
+    toast.error("Failed to download image. Please try again.");
+  }
+};
+
 
   return (
     <>
       <div className="gallery Flex">
+      <ToastContainer position="bottom-right" autoClose={2000} />
         <div className="gallery_container">
           {!wrongId && eventPinVerified ? (
             <>
